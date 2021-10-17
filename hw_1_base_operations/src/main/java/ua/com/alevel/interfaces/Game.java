f64e3d2c85f6c2180b47c769d50c9b8a61c56503
batievskiy <batievskiy.e.v@gmail.com>
2021-10-17 19:04:01 +0300

    Final refactoring

--- hw_1_base_operations/src/main/java/ua/com/alevel/interfaces/Game.java
+++ hw_1_base_operations/src/main/java/ua/com/alevel/interfaces/Game.java
@@ -3,6 +3,6 @@ package ua.com.alevel.interfaces;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-public interface GameInterface {
+public interface Game {
     void play(BufferedReader bufferedReader) throws IOException;
 }
