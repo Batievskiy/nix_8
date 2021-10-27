dc39b787351c1ddcd9e423714a89337f464af4ce
batievskiy <batievskiy.e.v@gmail.com>
2021-10-27 15:34:36 +0300

    fix bat files

--- hw_2_strings/deploy_hw_2_strings.bat
+++ hw_2_strings/deploy_hw_2_strings.bat
@@ -1,3 +1,7 @@
+call cd ../
+call cd ./reversestring
 call mvn clean install
+call cd ../
+call cd ./hw_2_strings
 call java -jar target\hw_2_strings.jar
 call pause
--- hw_2_strings/reversestring_clean_install.bat
+++ hw_2_strings/reversestring_clean_install.bat
@@ -3,4 +3,3 @@ call cd ./reversestring
 call mvn clean install
 call cd ../
 call cd ./hw_2_strings
-call pause
